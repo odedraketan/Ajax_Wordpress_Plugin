8beb75bbe23cf67563f6582aba72397b95833fac
odedraketan <ode92.ketan@gmail.com>
2025-11-10 16:36:15 +0530

    Update ajax-house-and-land.php

--- ajax-house-and-land.php
+++ ajax-house-and-land.php
@@ -1,232 +1,204 @@
 <?php
 /**
  * Plugin Name: House and Land
- * Description: Allows users to Search House and Land
- * Version: 1.0.0
+ * Description: Allows users to search House and Land packages via AJAX.
+ * Version: 1.1.0
  * Author: Coopso
  */
 
-add_action( 'wp_enqueue_scripts', 'ajax_test_enqueue_scripts' );
-function ajax_test_enqueue_scripts() {
-
-	wp_enqueue_script( 'ajax_test', plugins_url( '/script.js', __FILE__ ), array('jquery'), '1.0', true );
-
-	wp_localize_script( 'ajax_test', 'handl', array(
-		'ajax_url' => admin_url( 'admin-ajax.php' )
-	));
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Prevent direct access.
 }
+
+/**
+ * Enqueue frontend script with AJAX parameters.
+ */
+add_action( 'wp_enqueue_scripts', 'handl_enqueue_scripts' );
+function handl_enqueue_scripts() {
+	wp_enqueue_script(
+		'handl-ajax',
+		plugins_url( 'script.js', __FILE__ ),
+		array( 'jquery' ),
+		'1.1.0',
+		true
+	);
+
+	wp_localize_script(
+		'handl-ajax',
+		'handl',
+		array(
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
+			'nonce'    => wp_create_nonce( 'handl_nonce' ),
+		)
+	);
+}
+
+/**
+ * AJAX hook for both logged in and guest users.
+ */
 add_action( 'wp_ajax_nopriv_house_and_land_post', 'house_and_land_post' );
 add_action( 'wp_ajax_house_and_land_post', 'house_and_land_post' );
-//house_and_land_post
-function house_and_land_post() { ?>
-	<!-- results -->
-	<div class="results">
 
+/**
+ * Handle AJAX request for house and land search.
+ */
+function house_and_land_post() {
+	check_ajax_referer( 'handl_nonce', 'nonce' );
+
+	// Sanitize and parse request parameters
+	$price_range       = isset( $_GET['price'] ) ? array_map( 'floatval', explode( ',', sanitize_text_field( $_GET['price'] ) ) ) : [ 0, 700000 ];
+	$lot_width_range   = isset( $_GET['lotwidth'] ) ? array_map( 'floatval', explode( ',', sanitize_text_field( $_GET['lotwidth'] ) ) ) : [ 0, 100 ];
+	$bedroom_range     = isset( $_GET['bedroomrange'] ) ? array_map( 'intval', explode( ',', sanitize_text_field( $_GET['bedroomrange'] ) ) ) : [ 0, 10 ];
+	$bathroom_range    = isset( $_GET['bathroomrange'] ) ? array_map( 'intval', explode( ',', sanitize_text_field( $_GET['bathroomrange'] ) ) ) : [ 0, 10 ];
+	$orderby           = sanitize_text_field( $_GET['orderby'] ?? 'meta_value_num' );
+	$order             = strtoupper( sanitize_text_field( $_GET['order'] ?? 'ASC' ) );
+	$paged             = isset( $_GET['paged'] ) ? absint( $_GET['paged'] ) : 1;
+
+	// Query arguments
+	$args = array(
+		'post_type'      => 'house_and_land',
+		'post_status'    => 'publish',
+		'posts_per_page' => 6,
+		'paged'          => $paged,
+		'orderby'        => $orderby,
+		'order'          => in_array( $order, [ 'ASC', 'DESC' ], true ) ? $order : 'ASC',
+		'meta_key'       => 'price',
+		'meta_query'     => array(
+			'relation' => 'AND',
+			array(
+				'key'     => 'price',
+				'value'   => array( $price_range[0], $price_range[1] ),
+				'type'    => 'NUMERIC',
+				'compare' => 'BETWEEN',
+			),
+			array(
+				'key'     => 'lot_width',
+				'value'   => array( $lot_width_range[0], $lot_width_range[1] ),
+				'type'    => 'NUMERIC',
+				'compare' => 'BETWEEN',
+			),
+			array(
+				'key'     => 'bedroom',
+				'value'   => array( $bedroom_range[0], $bedroom_range[1] ),
+				'type'    => 'NUMERIC',
+				'compare' => 'BETWEEN',
+			),
+			array(
+				'key'     => 'bathroom',
+				'value'   => array( $bathroom_range[0], $bathroom_range[1] ),
+				'type'    => 'NUMERIC',
+				'compare' => 'BETWEEN',
+			),
+		),
+	);
+
+	$query = new WP_Query( $args );
+
+	ob_start();
+	?>
+	<div class="results">
 		<div class="results-list">
 			<div class="grid-container">
-
 				<div class="row">
-
-					<?php
-
-					if ($_GET['price']) {
-						$price = explode(',', $_GET['price']);
-						$price_min = $price[0];
-						$price_max = $price[1];
-					} else {
-						$price_min = 0;
-						$price_max = 700000;
-					}
-
-					if ($_GET['lotwidth']) {
-						$lotwidth = explode(',', $_GET['lotwidth']);
-						$lotwidth_min = $lotwidth[0];
-						$lotwidth_max = $lotwidth[1];
-					} else {
-						$lotwidth_min = 0;
-						$lotwidth_max = 100;
-					}
-
-					if ($_GET['bedroomrange']) {
-						$bedroomrange = explode(',', $_GET['bedroomrange']);
-						$bedroomrange_min = $bedroomrange[0];
-						$bedroomrange_max = $bedroomrange[1];
-					} else {
-						$bedroomrange_min = 0;
-						$bedroomrange_max = 10;
-					}
-
-					if (isset($_GET['bathroomrange'])) {
-						$bathroomrange = explode(',', $_GET['bathroomrange']);
-						$bathroomrange_min = $bathroomrange[0];
-						$bathroomrange_max = $bathroomrange[1];
-					} else {
-						$bathroomrange_min = 0;
-						$bathroomrange_max = 10;
-					}
-
-					if (isset($_GET['orderby'])) {
-						$orderby = $_GET['orderby'];
-					} else {
-						$orderby = 'meta_value_num';
-					}
-
-					if (isset($_GET['order'])) {
-						$order = $_GET['order'];
-					} else {
-						$order = 'ASC';
-					}
-
-					$query = new WP_Query(array(
-							'post_type' => 'house_and_land',
-							'post_status' => 'publish',
-							'posts_per_page' => 6,
-							'orderby' => $orderby,
-							'order' => $order,
-							'paged' => get_query_var('paged'),
-							'meta_key' => 'price',
-							'meta_query' => array(
-									'relation' => 'AND',
-									array(
-											'key'     => 'price',
-											'value' => array( $price_min, $price_max ),
-											'type' => 'numeric',
-											'compare' => 'BETWEEN'
-									),
-									array(
-											'key'     => 'lot_width',
-											'value' => array( $lotwidth_min, $lotwidth_max ),
-											'type' => 'numeric',
-											'compare' => 'BETWEEN'
-									),
-									array(
-											'key'     => 'bedroom',
-											'value' => array( $bedroomrange_min, $bedroomrange_max ),
-											'type' => 'numeric',
-											'compare' => 'BETWEEN'
-									),
-									array(
-											'key'     => 'bathroom',
-											'value' => array( $bathroomrange_min, $bathroomrange_max ),
-											'type' => 'numeric',
-											'compare' => 'BETWEEN'
-									)
-							)
-					));
-					?>
 					<div class="col-12 search_result">
-						<div class="search_result_title">Search results</div>
+						<div class="search_result_title">Search Results</div>
 						<div class="user_lot_result">
 							<?php
-							echo 'Price Range $'.number_format($price_min).' - $'.number_format($price_max).', Lot width '.$lotwidth_min.'m - '.$lotwidth_max. 'm, Bedroom '.$bedroomrange_min.'-'.$bedroomrange_max. ', Bathroom '.$bathroomrange_min.'-'.$bathroomrange_max;
+							printf(
+								'Price Range: <strong>$%s - $%s</strong>, Lot Width: <strong>%sm - %sm</strong>, Bedroom: <strong>%s-%s</strong>, Bathroom: <strong>%s-%s</strong>',
+								number_format_i18n( $price_range[0] ),
+								number_format_i18n( $price_range[1] ),
+								esc_html( $lot_width_range[0] ),
+								esc_html( $lot_width_range[1] ),
+								esc_html( $bedroom_range[0] ),
+								esc_html( $bedroom_range[1] ),
+								esc_html( $bathroom_range[0] ),
+								esc_html( $bathroom_range[1] )
+							);
 							?>
 						</div>
 					</div>
-					<?php
-					$special_off='';
-					$paged=null;
-					if ( $query->have_posts() ) {
-						while ($query->have_posts()) {
-							$query->the_post();
 
-							$thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium' );
-							$thumb = $thumb['0'];
-
-							$price = get_field('price');
-							$price = number_format($price,0,'.',',');
+					<?php if ( $query->have_posts() ) : ?>
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+							<?php
+							$thumb = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
+							$price = get_field( 'price' );
+							$price = $price ? number_format_i18n( $price ) : '';
+							$lot_text = get_field( 'lot_text' );
 							?>
-							<!-- item -->
 							<div class="col-4 results-list-view">
-
 								<div class="results-list-item grid">
+									<a href="<?php echo esc_url( get_field( 'brochure' ) ); ?>" target="_blank" rel="noopener">
+										<div class="results-list-item-image" style="background-image: url('<?php echo esc_url( $thumb ); ?>')"></div>
+									</a>
 
-									<a href="<?php echo get_field('brochure'); ?>" target="_blank"> <div class="results-list-item-image" style="background-image: url(<?php echo $thumb; ?>)"></div></a>
 									<div class="results-list-item-desc equal">
-
-										<div class="results-list-item-desc-head house">
-											<div class="title text-center">
-												<?php echo get_the_title(); ?>
-											</div>
-											<div class="price text-center">
-												<span class="from">From</span> <?php if($special_off != ''){ ?>
-													<span style="color: #d80000;text-decoration:line-through">
-												<span style="color: #333;"><?php echo '$'.$price; ?></span>
-											</span>
-												<?php } else{ ?>
-													<?php echo '$'.$price; ?>
-												<?php }  ?>
+										<div class="results-list-item-desc-head house text-center">
+											<div class="title"><?php the_title(); ?></div>
+											<div class="price">
+												<span class="from">From</span> $<?php echo esc_html( $price ); ?>
 											</div>
 										</div>
 
-										<?php if(get_field('lot_text')){ ?>
+										<?php if ( $lot_text ) : ?>
 											<div class="results-list-item-desc-lot">
 												<div class="results-list-item-desc-lot-text text-center">
-													<?php  the_field('lot_text'); ?>
+													<?php echo esc_html( $lot_text ); ?>
 												</div>
 											</div>
-
-										<?php } ?>
+										<?php endif; ?>
 
 										<div class="results-list-item-desc-icons text-center">
 											<div class="cell">
-												<div class="cell-image">
-													<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-4.png" alt="" />
-												</div>
-												<div class="cell-label">
-													<?php the_field('bedroom'); ?> Bed
-												</div>
+												<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/icon-4.png' ); ?>" alt="Bed">
+												<div class="cell-label"><?php echo esc_html( get_field( 'bedroom' ) ); ?> Bed</div>
 											</div>
 											<div class="cell">
-												<div class="cell-image">
-													<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-5.png" alt="" />
-												</div>
-												<div class="cell-label">
-													<?php the_field('bathroom'); ?> Bath
-												</div>
+												<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/icon-5.png' ); ?>" alt="Bath">
+												<div class="cell-label"><?php echo esc_html( get_field( 'bathroom' ) ); ?> Bath</div>
 											</div>
 											<div class="cell">
-												<div class="cell-image">
-													<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-3.png" alt="" />
-												</div>
-												<div class="cell-label">
-													<?php the_field('garage'); ?> Car
-												</div>
+												<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/icon-3.png' ); ?>" alt="Car">
+												<div class="cell-label"><?php echo esc_html( get_field( 'garage' ) ); ?> Car</div>
 											</div>
 										</div>
 
 										<div class="house_buttons">
-											<div class="find_land"><a href="<?php echo get_permalink(94); ?>?lotwidth=0, <?php echo get_field('lot_width')?>"><button class="button"><img src="<?php echo get_template_directory_uri() ?>/assets/img/button_home.png"> Find land <i class="fa fa-arrow-right" aria-hidden="true"></i></button></a></div>
-											<div class="enquire">
-												<a href="#" data-content="<?php if(get_field('lot_text')){ echo 'Lot: '.get_field('lot_text').' House: '.get_the_title(); }else { echo get_the_title(); } ?>" class="js-open-enquire button"><i class="fa fa-commenting-o" aria-hidden="true"></i> Enquire <i class="fa fa-arrow-right" aria-hidden="true"></i> </a>
+											<div class="find_land">
+												<a href="<?php echo esc_url( get_permalink( 94 ) . '?lotwidth=0,' . get_field( 'lot_width' ) ); ?>">
+													<button class="button">
+														<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/button_home.png' ); ?>" alt=""> 
+														Find Land <i class="fa fa-arrow-right" aria-hidden="true"></i>
+													</button>
+												</a>
 											</div>
-
+											<div class="enquire">
+												<a href="#" data-content="<?php echo esc_attr( $lot_text ? 'Lot: ' . $lot_text . ' House: ' . get_the_title() : get_the_title() ); ?>" class="js-open-enquire button">
+													<i class="fa fa-commenting-o" aria-hidden="true"></i> Enquire <i class="fa fa-arrow-right" aria-hidden="true"></i>
+												</a>
+											</div>
 										</div>
 
-										<div class="results-list-item-desc-links">
-											<img src=<?php echo get_field('builder_logo')?> >
-										</div>
-
+										<?php if ( get_field( 'builder_logo' ) ) : ?>
+											<div class="results-list-item-desc-links">
+												<img src="<?php echo esc_url( get_field( 'builder_logo' ) ); ?>" alt="Builder Logo">
+											</div>
+										<?php endif; ?>
 									</div>
-
 								</div>
 							</div>
-							<!-- /item -->
+						<?php endwhile; ?>
 
-						<?php } wp_reset_query();
-
-					} else { ?>
-
-						<h2 class="no-match">Nothing Found Sorry, no posts matched your criteria.</h2>
-
-					<?php } ?>
+					<?php else : ?>
+						<h2 class="no-match">Nothing Found — Sorry, no posts matched your criteria.</h2>
+					<?php endif; ?>
 
 				</div>
-
 				<?php wp_reset_postdata(); ?>
 			</div>
 		</div>
-
 	</div>
-	<!-- /results -->
-<?php } ?>
+	<?php
+	wp_send_json_success( ob_get_clean() );
+}
